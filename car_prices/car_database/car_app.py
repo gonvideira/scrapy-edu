6dbc278d825ac8431d9b500d19f0d4c6c6fc06b4
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-09-07 15:40:43 +0100

    corrected sql

--- car_prices/car_database/car_app.py
+++ car_prices/car_database/car_app.py
@@ -4,12 +4,12 @@ from car_database import CarDatabase
 db_connection = CarDatabase()
 
 # SETTING UP THE DATABASE
-db_connection.create_table_cars()
-db_connection.create_table_ads()
-db_connection.create_table_prices()
+# db_connection.create_table_cars()
+# db_connection.create_table_ads()
+# db_connection.create_table_prices()
 
 db_connection.update_db('cars.json')
 
-# db_connection.show_all('cars')
+db_connection.show_all('cars')
 
 db_connection.close_connection()
